de1a0668b90737f1849f38f49c0e02c17456b370
Douglas Blank <doug.blank@gmail.com>
2026-05-03 22:02:07 -0400

    Update gramps/plugins/gramplet/givennamegramplet.py
    Co-authored-by: ClmntPnd <clement.pinard04@gmail.com>

--- gramps/plugins/gramplet/givennamegramplet.py
+++ gramps/plugins/gramplet/givennamegramplet.py
@@ -55,4 +55,31 @@ class GivenNameCloudGramplet(CloudGramplet):
             for name in allnames:
                 name = name.get_first_name().strip()
                 items.append((name, name))
-        return items
+    def init(self):
+        CloudGramplet.init(self)
+        self.set_value_name("given name")
+        self.set_preference_no_value("preferences.no-given-text")
+        self.set_tooltip(_("Click surname to view people with that surname"))
+
+    def on_item_clicked(self, word, linked_data):
+        run_quick_report_by_name(
+            self.dbstate, self.uistate, "samegivens_misc", linked_data
+        )
+
+    def db_changed(self):
+        self.connect(self.dbstate.db, "person-add", self.update)
+        self.connect(self.dbstate.db, "person-delete", self.update)
+        self.connect(self.dbstate.db, "person-update", self.update)
+        self.connect(self.dbstate.db, "person-rebuild", self.update)
+        self.connect(self.dbstate.db, "family-rebuild", self.update)
+
+    def get_items(self):
+        counts = {}
+        for person in self.dbstate.db.iter_people():
+            allnames = [person.get_primary_name()] + person.get_alternate_names()
+            for name in allnames:
+                given_name = name.get_first_name().strip()
+                if self.filter_missing and (not given_name or given_name == "?"):
+                    continue
+                counts[given_name] = counts.get(given_name, 0) + 1
+        return [(given_name, given_name, counts[given_name]) for given_name in counts]
